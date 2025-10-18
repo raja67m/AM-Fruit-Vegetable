d360a098daa8be0b9772eb0f38fe2f41999e0368
raja <rajalingam210901@gmail.com>
2025-10-18 06:21:01 +0530

    responsive nav bar design

---
+++ navbar.js
@@ -0,0 +1,12 @@
+function resNavBar(){
+document.querySelector('.fi-br-menu-burger').style.display="none";
+document.getElementById('close').style.display="block";
+document.getElementById('mobRes').style.display="block";
+console.log(resNavBar)
+}
+
+function closeNavBar(){
+   document.querySelector('.fi-br-menu-burger').style.display="block";
+   document.getElementById('close').style.display="none";
+  
+}
