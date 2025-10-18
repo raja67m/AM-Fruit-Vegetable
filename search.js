d7f7e5d6a4d2b80dd09f83e9ad0807d959e4aacd
raja <rajalingam210901@gmail.com>
2025-10-18 06:03:11 +0530

    search function compeleted

---
+++ search.js
@@ -0,0 +1,18 @@
+function searchBar(){
+   let input=document.getElementById('searchBar').value;
+    input=input.toLowerCase();
+    let x=document.querySelectorAll(".products");
+ 
+    for(i=0;i<x.length;i++){
+      if(input === ''){
+         x[i].style.display='';
+
+      }else if(!x[i].innerHTML.toLowerCase().includes(input)){
+         x[i].style.display="none";
+        
+      }else{
+         x[i].style.display='products';
+      
+      }
+    }
+}
