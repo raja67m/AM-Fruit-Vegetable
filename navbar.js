241b7a3a6b0bcd901aa223b922d7f52c981aa9b7
raja <rajalingam210901@gmail.com>
2025-10-22 18:13:37 +0530

    responsive nav style compeleted

--- navbar.js
+++ navbar.js
@@ -2,11 +2,11 @@ function resNavBar(){
 document.querySelector('.fi-br-menu-burger').style.display="none";
 document.getElementById('close').style.display="block";
 document.getElementById('mobRes').style.display="block";
-console.log(resNavBar)
+
 }
 
 function closeNavBar(){
    document.querySelector('.fi-br-menu-burger').style.display="block";
    document.getElementById('close').style.display="none";
-  
+   document.getElementById('mobRes').style.display="none";
 }
